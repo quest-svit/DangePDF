2c8f1f2feaa59d25141e3d838d7f78be35b5d84e
tanmay <quest_svit@yahoo.co.in>
2022-03-28 12:50:46 +0530

    Added PDF ZoomIn and ZoomOut capabilities

--- src/pdfViewer.py
+++ src/pdfViewer.py
@@ -3,7 +3,7 @@ import PyPDF2
 from tkinter import *
 from tkinter import filedialog
 from pdf2image import convert_from_path
-import tkPDFViewer as pdf
+import mytkPDFViewer as pdf
 
 #Create an instance of tkinter frame
 win= Tk()
@@ -23,6 +23,8 @@ scrol_y.pack(side=RIGHT,fill=Y)
 # set global variable v2
 global pdf_frame2,zoomDPI,zoomDPIdefault,file
 pdf_frame2 = None
+zoomDPI,zoomDPIdefault=72,72
+
 
 #Define a function to clear the frame
 def clear_frame():
@@ -45,7 +47,7 @@ def open_pdf():
       # Adding pdf location and width and height.
       pdf_frame2 = v1.pdf_view(pdf_frame,
                      pdf_location = file, 
-                     width = 1500, height = 2000)
+                     width = 1500, height = 2000, zoomDPI=zoomDPIdefault)
       
       # Placing Pdf in my gui.
       pdf_frame2.pack()
@@ -54,6 +56,79 @@ def open_pdf():
 # Define function to Quit the window
 def quit_app():
    win.destroy()
+
+
+def zoomIn():
+    try:
+        global zoomDPI,file,pdf_frame2
+        zoomDPI=int(zoomDPI*1.5)
+
+        if pdf_frame2: # if old instance exists, destroy it first
+            pdf_frame2.destroy()
+        v1 = pdf.ShowPdf() 
+        
+        # clear the image list # this corrects the bug inside tkPDFViewer module
+        v1.img_object_li.clear()
+        
+        # Adding pdf location and width and height. 
+        pdf_frame2 = v1.pdf_view(pdf_frame,
+                    pdf_location = file, 
+                    width = 1500, height = 2000, zoomDPI=zoomDPI)
+        
+        # Placing Pdf inside gui
+        pdf_frame2.pack()
+        mainloop()
+    
+    except:
+        pass
+    
+def zoomOut():
+    try:
+        global zoomDPI,pdf_frame2,file
+        zoomDPI=int(zoomDPI*0.5)
+
+        if pdf_frame2: # if old instance exists, destroy it first
+            pdf_frame2.destroy()
+        
+        # creating object of ShowPdf from tkPDFViewer. 
+        v1 = pdf.ShowPdf() 
+        
+        # clear the image list # this corrects the bug inside tkPDFViewer module
+        v1.img_object_li.clear()
+        pdf.ShowPdf().img_object_li.clear()
+
+        # Adding pdf location and width and height. 
+        pdf_frame2=v1.pdf_view(pdf_frame,pdf_location = file, zoomDPI=zoomDPI)
+        
+        # Placing Pdf inside gui
+        pdf_frame2.pack()
+        mainloop()
+    except:
+        pass
+
+def zoomRestore():
+    try:
+        global zoomDPI,zoomDPIdefault,pdf_frame2,file
+        zoomDPI=zoomDPIdefault
+        # messagebox.showinfo( "Hello Python", "Hello World")
+        if pdf_frame2: # if old instance exists, destroy it first
+            pdf_frame2.destroy()
+        
+        # creating object of ShowPdf from tkPDFViewer. 
+        v1 = pdf.ShowPdf() 
+        
+        # clear the image list # this corrects the bug inside tkPDFViewer module
+        v1.img_object_li.clear()
+        
+        # Adding pdf location and width and height. 
+        pdf_frame2=v1.pdf_view(pdf_frame,pdf_location = file, zoomDPI=zoomDPI)
+        
+        # Placing Pdf inside gui
+        pdf_frame2.pack()
+        mainloop()
+    except:
+        pass
+
 
 
 # Create a Menu
@@ -70,5 +145,10 @@ my_menu.add_cascade(label="Edit",menu= edit_menu)
 file_menu.add_command(label="Open",command=open_pdf)
 file_menu.add_command(label="Clear",command=clear_frame)
 file_menu.add_command(label="Quit",command=quit_app)
+
+edit_menu.add_command(label="ZoomIn",command=zoomIn)
+edit_menu.add_command(label="ZoomOut",command=zoomOut)
+edit_menu.add_command(label="ZoomRestore",command=zoomRestore)
+
 
 win.mainloop()
